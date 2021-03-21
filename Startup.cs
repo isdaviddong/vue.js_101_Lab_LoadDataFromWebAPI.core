a3952d68272fad0db1339171b08f31a04c962e1f
David <twdeveloper@hotmail.com>
2021-03-21 16:32:17 +0800

    add razor page to work

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace test01
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace test01
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
